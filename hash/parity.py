f57978d1e9d53333844fabd9a76c6258f7b2c94a
Zach Lee <zach.lee@concur.com>
2018-04-09 09:22:10 -0700

    Add docstring

--- hash/parity.py
+++ hash/parity.py
@@ -1,6 +1,11 @@
 from . import base
 
 class Parity(base.HashFunction):
+    """
+    Hash function that returns 0 if the word has an even number of
+    characters and 1 if it has an odd number of characters.
+    """
+
     def hash(self, string):
         is_even = len(string) % 2 == 0
         if is_even:
